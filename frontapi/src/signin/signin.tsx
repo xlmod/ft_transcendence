6503d242b6b2f9454c300de93e3c133690330b71
macasubo <macasubo@student.42.fr>
2022-08-09 18:16:07 +0200

    /game redirection in dev mode

--- frontapi/src/signin/signin.tsx
+++ frontapi/src/signin/signin.tsx
@@ -13,7 +13,9 @@ export function Signin()
 				{/* <button id="signin" onClick={ getToken }>
 					<p>Sign in</p>
 				</button> */}
-				<a id="signin" href="http://localhost:3333/auth/42/callback"> Sin</a>
+			<a id="signin" href="http://localhost:3333/auth/42/callback">
+				<p>Signin</p>
+			</a>
 			</section>
 		</main>
 	);
