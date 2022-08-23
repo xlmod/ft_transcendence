79ed138d3cae6ff5a8be408cee6f2a1fd2caaed6
macasubo <macasubo@student.42.fr>
2022-08-23 19:54:05 +0200

    added QR related functions in middleware

--- frontapi/src/index.tsx
+++ frontapi/src/index.tsx
@@ -2,21 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { Header } from './header/header';
 import { Signin } from './signin/signin';
 import { Game } from './game/game';
 
-export default function Main() {
-	return(
+/*
+TODO
+
+
+*/
+
+class Main extends React.Component {
+	render()
+	{
+		return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={ <Signin /> }>
-					<Route path="signin" element={ <Signin /> } />
-					<Route path="game" element={ <Game /> } />
-				</Route>
+				<Route path="/" element={ <Signin /> } />
+				<Route path="signin" element={ <Signin /> } />
+				<Route path="game" element={ <Game /> } />
 			</Routes>
 		</BrowserRouter>
-	);
+		);
+	}
 }
 
 /*<Route path="*" element={ <Error404 /> } />*/
--- frontapi/src/signin/signin.tsx
+++ frontapi/src/signin/signin.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Header } from '../header/header';
-import { getToken } from '../middleware';
 import './signin.css';
 
 export function Signin()
