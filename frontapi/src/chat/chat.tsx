4df3c238c4ae71cc11c55767f9e773141d0f3164
macasubo <macasubo@student.42.fr>
2022-08-26 22:52:54 +0200

    added chat divs

--- frontapi/src/chat/chat.tsx
+++ frontapi/src/chat/chat.tsx
@@ -1,6 +1,8 @@
 import { Header } from '../header/header';
 import { Navbar } from '../navbar/navbar';
 
+import './chat.css';
+
 export function Chat()
 : JSX.Element
 {
@@ -8,6 +10,19 @@ export function Chat()
 		<main>
 			<Header />
 			<Navbar />
+			<section id="chatSection">
+				<div id="friends">
+
+				</div>
+				<div id="chatParent">
+					<div id="chat">
+
+					</div>
+				</div>
+				<div id="channels">
+
+				</div>
+			</section>
 		</main>
 	);
 }
