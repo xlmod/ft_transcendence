73a24638aade1592cab9738e1d68910584173eb0
xlmod <glafond-@student.42.fr>
2022-09-01 23:50:15 +0200

    Upgrade font with hacked version and chat css:
    * Work on css of the message div in chat.
    * Download font to use number a special charactere.

--- frontapi/src/components/chat/chat.tsx
+++ frontapi/src/components/chat/chat.tsx
@@ -30,21 +30,53 @@ export function Chat()
 				<div id="chatParent">
 					<div id="chat">
 					<div id="messages">
-						<p className="messages">
+						<p className="messages other">
 							<span className="sender">Princess Leïa__</span>
-							<span className="body">Wesh bien ?</span>
+							<br/>
+							<span className="body">
+								Wesh bien ?
+								<span className="date">15:03</span>
+							</span>
 						</p>
-						<p className="messages">
+						<p className="messages me">
 							<span className="sender">Me__</span>
-							<span className="body">42</span>
+							<br/>
+							<span className="body">
+								ok
+								<span className="date">15:03</span>
+							</span>
 						</p>
-						<p className="messages">
+						<p className="messages other">
 							<span className="sender">Princess Leïa__</span>
-							<span className="body">Sinon quoi de neuf ?</span>
+							<br/>
+							<span className="body">
+								Sinon quoi de neuf ?
+								<span className="date">15:03</span>
+							</span>
 						</p>
-						<p className="messages">
+						<p className="messages me">
 							<span className="sender">Me__</span>
-							<span className="body">42</span>
+							<br/>
+							<span className="body">
+								The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.)
+								<span className="date">15:03</span>
+							</span>
+						</p>
+						<p className="messages other">
+							<span className="sender">Princess Leïa__</span>
+							<br/>
+							<span className="body">
+								Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.
+								<span className="date">15:03</span>
+							</span>
+						</p>
+						<p className="messages me">
+							<span className="sender">Me__</span>
+							<br/>
+							<span className="body">
+								The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum. 
+								<span className="date">15:03</span>
+							</span>
 						</p>
 
 					</div>
