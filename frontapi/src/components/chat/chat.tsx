7296654f4216ee84a6a1ede90c48a8bbe64cfb90
Gabriel LT <glafond-@student.42.fr>
2022-09-02 15:28:50 +0000

    Try new design (#11)
    No boarder except for the main chat.
    Add a shadow when scroll up.
    Remove scroll bar of the friend list.
    Set friends, members and rooms element to the same size.
    Add a little bit of JS to add new message (for test purpose).

--- frontapi/src/components/chat/chat.tsx
+++ frontapi/src/components/chat/chat.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { AuthContext } from '../../services/auth.service';
 
 import './chat.css';
@@ -6,82 +6,76 @@ import './chat.css';
 export function Chat()
 : JSX.Element
 {
+
 	const {checkLogin} = useContext(AuthContext);
 	checkLogin();
+
+	const inRef = useRef<HTMLInputElement | null>(null);
+
+	const [list,setList] = useState<[JSX.Element]>([Message({date:"", owner:"", me:false, body:"Welcome to the chat zone"})]);
+	const [,updateState] = useState<{}>();
+
+	const addMessage = (event:React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const d = new Date();
+		let input: HTMLInputElement | null = inRef.current;
+		if (input) {
+			let value = input.value;
+			if (value.trimStart() === "")
+				return;
+			input.value = "";
+			const msg = Message({date:d.toLocaleTimeString(), owner:"Me", me: true, body:value});
+			let l = list;
+			l?.push(msg);
+			setList(l);
+			updateState({});
+		}
+	};
+
 	return (
 		<main>
 			<section id="chatSection">
-			<div id="users">
-				<div id="friends">
-					<h1>Friends</h1>
-					<ul>
-						<li className="away">Yoda</li>
-						<li className="playing">Anakin Skywalker</li>
-						<li className="connected">Princess Leïa</li>
-					</ul>
+				<div id="users">
+					<div id="friends">
+						<h1>Friends</h1>
+						<ul>
+							<li className="away">Yoda</li>
+							<li className="playing">Anakin Skywalker</li>
+							<li className="connected">Princess Leïa</li>
+							<li className="away">Yoda</li>
+							<li className="away">Yoda</li>
+							<li className="away">Yoda</li>
+							<li className="away">Yoda</li>
+							<li className="away">Yoda</li>
+							<li className="away">Yoda</li>
+							<li className="playing">Anakin Skywalker</li>
+							<li className="connected">Princess Leïa</li>
+							<li className="playing">Anakin Skywalker</li>
+							<li className="connected">Princess Leïa</li>
+							<li className="playing">Anakin Skywalker</li>
+							<li className="connected">Princess Leïa</li>
+							<li className="playing">Anakin Skywalker</li>
+							<li className="connected">Princess Leïa</li>
+							<li className="playing">Anakin Skywalker</li>
+							<li className="connected">Princess Leïa</li>
+							<li className="playing">Anakin Skywalker</li>
+							<li className="connected">Princess Leïa</li>
+						</ul>
+					</div>
+					<div id="members">
+						<h1>Room members</h1>
+						<ul>
+							<li className="connected">Princess Leïa</li>
+						</ul>
+					</div>
 				</div>
-				<div id="members">
-					<h1>Room members</h1>
-					<ul>
-						<li className="connected">Princess Leïa</li>
-					</ul>
-				</div>
-			</div>
 				<div id="chatParent">
 					<div id="chat">
 					<div id="messages">
-						<p className="messages other">
-							<span className="sender">Princess Leïa__</span>
-							<br/>
-							<span className="body">
-								Wesh bien ?
-								<span className="date">15:03</span>
-							</span>
-						</p>
-						<p className="messages me">
-							<span className="sender">Me__</span>
-							<br/>
-							<span className="body">
-								ok
-								<span className="date">15:03</span>
-							</span>
-						</p>
-						<p className="messages other">
-							<span className="sender">Princess Leïa__</span>
-							<br/>
-							<span className="body">
-								Sinon quoi de neuf ?
-								<span className="date">15:03</span>
-							</span>
-						</p>
-						<p className="messages me">
-							<span className="sender">Me__</span>
-							<br/>
-							<span className="body">
-								The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.)
-								<span className="date">15:03</span>
-							</span>
-						</p>
-						<p className="messages other">
-							<span className="sender">Princess Leïa__</span>
-							<br/>
-							<span className="body">
-								Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.
-								<span className="date">15:03</span>
-							</span>
-						</p>
-						<p className="messages me">
-							<span className="sender">Me__</span>
-							<br/>
-							<span className="body">
-								The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum. 
-								<span className="date">15:03</span>
-							</span>
-						</p>
-
+						{list}
 					</div>
-						<form>
-							<input type="text" name="message" placeholder="Type your message here"/>
+						<form onSubmit={addMessage}>
+							<input type="text" name="message" placeholder="Type your message here" ref={inRef} />
 							<input type="submit" value="Send"/>
 						</form>
 					</div>
@@ -99,3 +93,36 @@ export function Chat()
 		</main>
 	);
 }
+
+function Message(props: {date: string, owner: string, me:boolean, body: string})
+: JSX.Element
+{
+
+	if (props.me) {
+		return (
+				<p className="messages me">
+					<span className="sender">{props.owner}</span>
+					<br/>
+					<span className="body">
+						{props.body}
+						<span className="date">{props.date}</span>
+					</span>
+				</p>
+		);
+	} else {
+		return (
+				<p className="messages other">
+					<span className="sender">{props.owner}</span>
+					<br/>
+					<span className="body">
+						{props.body}
+						<span className="date">{props.date}</span>
+					</span>
+				</p>
+		);
+	}
+}
+
+/*
+						{ Message({date:"16:09", owner:"Me", me:true, body:"The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum."})}
+						*/
