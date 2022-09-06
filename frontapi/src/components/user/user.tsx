c288dff2486fecca4606fa28e5263ef77ecf50b1
xlmod <glafond-@student.42.fr>
2022-09-06 12:43:09 +0200

    Change User from class to function component.

--- frontapi/src/components/user/user.tsx
+++ frontapi/src/components/user/user.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Navigate} from 'react-router';
+
 import {iaxios} from "../../utils/axios";
+import { AuthContext } from '../../services/auth.service';
 
 import './user.css';
 
@@ -16,101 +18,91 @@ interface IState {
 	edit: boolean,
 }
 
-export class User extends React.Component< IProps, IState >
-{
-	constructor(props: IProps)
-	{
-		super(props);
-		this.state = {
-			user: [],
-			uid: "",
-			connected: true,
-			edit: false,
-		};
+export function User(props: IProps) {
 
-		this.updateUserData = this.updateUserData.bind(this);
-		this.closeUserEdit = this.closeUserEdit.bind(this);
-	}
+	const {checkLogin} = useContext(AuthContext);
+	checkLogin();
 
-	getUid = async () => {
+	const [user, setUser] = useState<any>([]);
+	const [uid, setUid] = useState<string>("");
+	const [connected, setConnected] = useState<boolean>(true);
+	const [edit, setEdit] = useState<boolean>(false);
+	const [update, setUpdate] = useState<boolean>(false);
+
+
+	const getUid = async () => {
 		return await iaxios.get('/user/me')
 			.then( (data) => {
 				return data.data.uid;
 			}).catch( () => {return ""});
 	};
-	getUser = async (uid: string) => {
+	const getUser = async (uid: string) => {
 		return await iaxios.get('/user/' + uid)
 			.then( (data) => {
 				return  data.data;
 			}).catch(() => {return []});
 	};
-	getAvatar = async (uid: string) => {
+	const getAvatar = async (uid: string) => {
 		return await iaxios.get('/profile/' + uid)
 			.then( (data) => {
 				return  data.data;
 			}).catch(() => {return []});
 	};
 
-	async updateUserData() {
-		let uid = await this.getUid();
-		let connected:boolean = false;
-		let user: any = {};
-		if (uid !== "") {
-			user = await this.getUser(uid);
-			connected = true;
+	const updateUserData = async () => {
+		let uid_l = await getUid();
+		let connected_l:boolean = false;
+		let user_l: any = {};
+		if (uid_l !== "") {
+			user_l = await getUser(uid_l);
+			connected_l = true;
 		}
-		console.log(await this.getAvatar(uid));
-		if (user !== this.state.user) {
-			this.setState({
-				user: user,
-				uid: uid,
-				connected: connected,
-			});
+		console.log(await getAvatar(uid_l));
+		if (user_l !== user) {
+			setUser(user_l);
+			setUid(uid_l);
+			setConnected(connected_l);
 		}
 	}
 
-	async componentDidMount() {
-		await this.updateUserData();
+	const closeUserEdit = (update: boolean) => {
+		setEdit(false);
 	}
 
+	useEffect(() => {
+		updateUserData();
+	}, [edit]);
 
-	closeUserEdit(update: boolean) {
-		this.setState({edit:false});
-		if (update)
-			this.updateUserData();
-	}
 
-	render() {
-		if (!this.state.connected)
-			return(<Navigate to="/signin" />);
-		return (
-			<main>
-				<section id="user-section">
-					{this.state.edit && <Useredit close={this.closeUserEdit} pseudo={this.state.user.pseudo} />}
-					<div id="user-id">
-						<div id="user-id-avatar">
-							<img src ={ this.state.user.avatar }/>
-						</div>
-						<div id="user-id-info">
-							<div id="user-id-name">
-								<p>{ this.state.user.firstName } {this.state.user.lastName}</p>
-							</div>
-							<div id="user-id-pseudo">
-								<p>{ this.state.user.pseudo }</p>
-							</div>
-							<div id="user-id-elo">
-								<p>{this.state.user.elo}</p>
-							</div>
-							<Button id="user-info-edit" value="edit info" fontSize={0.7} onClick={() => this.setState({edit:true})} />
-						</div>
+	if (!connected)
+		return(<Navigate to="/signin" />);
+	return (
+		<main>
+			<section id="user-section">
+				{edit && <Useredit close={closeUserEdit} pseudo={user.pseudo} />}
+				<div id="user-id">
+					<div id="user-id-avatar">
+						<img src ={ user.avatar }/>
 					</div>
-					<div id="user-matchhistory">
-						<div className="user-title">Match History</div>
-						<div className="user-list">
+					<div id="user-id-info">
+						<div id="user-id-name">
+							<p>{ user.firstName } {user.lastName}</p>
 						</div>
+						<div id="user-id-pseudo">
+							<p>{ user.pseudo }</p>
+						</div>
+						<div id="user-id-elo">
+							<p>{user.elo}</p>
+						</div>
+						<Button id="user-info-edit" value="edit info" fontSize={0.7} onClick={() => {setEdit(true)}} />
 					</div>
-				</section>
-			</main>
-		);
-	}
+				</div>
+				<div id="user-matchhistory">
+					<div className="user-title">Match History</div>
+					<div className="user-list">
+					</div>
+				</div>
+			</section>
+		</main>
+	);
 }
