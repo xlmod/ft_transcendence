7279ab124e53b49588c2768409bd7c9e337bbe60
macasubo <macasubo@student.42.fr>
2022-09-06 15:09:53 +0200

    users menu premier jet

--- frontapi/src/components/tfa/tfa.tsx
+++ frontapi/src/components/tfa/tfa.tsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 import { AuthContext } from '../../services/auth.service';
 import './tfa.css';
 
-const API_URL = "http://localhost:3333/"
+const API_URL = "http://localhost:3333/";
 
 export const TFA = () =>
 {
@@ -19,7 +19,7 @@ export const TFA = () =>
 		setInput( res );
 		if( res[0] && res[1] && res[2] && res[3] && res[4] && res[5] )
 		{
-			axios.get( API_URL + 'tfa', { withCredentials: true } )
+			axios.get( API_URL + 'tfa/authenticate', { withCredentials: true } )
 				.then( ( data :any ) => { } )
 				.catch( () => { setFailed( "fail" ); } );
 		}
