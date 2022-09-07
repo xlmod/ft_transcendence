5a2284f64f8b9cfcdd422f7fd3a699bbe08a6d61
macasubo <macasubo@student.42.fr>
2022-09-07 09:25:28 +0200

    some modifications to menuUsers

--- frontapi/src/components/tfa/tfa.tsx
+++ frontapi/src/components/tfa/tfa.tsx
@@ -1,12 +1,10 @@
 import React, { useContext, useState } from 'react';
 import NumericInput from 'react-numeric-input';
 import AuthCode from 'react-auth-code-input';
-import axios from 'axios';
+import { iaxios } from '../../utils/axios';
 
 import { AuthContext } from '../../services/auth.service';
 import './tfa.css';
-
-const API_URL = "http://localhost:3333/";
 
 export const TFA = () =>
 {
@@ -19,7 +17,7 @@ export const TFA = () =>
 		setInput( res );
 		if( res[0] && res[1] && res[2] && res[3] && res[4] && res[5] )
 		{
-			axios.get( API_URL + 'tfa/authenticate', { withCredentials: true } )
+			iaxios.post( 'tfa/authenticate' )
 				.then( ( data :any ) => { } )
 				.catch( () => { setFailed( "fail" ); } );
 		}
--- frontapi/src/components/utils/menu_users.tsx
+++ frontapi/src/components/utils/menu_users.tsx
@@ -1,30 +1,44 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import { NavLink } from 'react-router-dom';
+import { iaxios } from '../../utils/axios';
 import CSS from 'csstype';
 
 import './menu_users.css';
 
 const FRONT_URL = "http://localhost:3000/";
-const BACK_URL = "http://localhost:3333/relationship/";
+//const BACK_URL = "http://localhost:3333/relationship/";
 
 export const MenuUsers = ( id :string, isFriend :boolean, isBlocked :boolean ) => {
+
+	const clickFriend = () =>
+	{
+		iaxios.get( 'relationship/' + ( ( !isFriend ) ? 'add:' : 'remove:' ) + id )
+			.catch( () => { } );
+//		isFriend = !isFriend;
+	}
+
+	const clickBlock = () =>
+	{
+		iaxios.get( 'relationship/' + ( ( !isBlocked ) ? 'block:' : 'unblock:' ) + id )
+			.catch( () => { } );
+//		isBlocked = !isBlocked;
+	}
+
 	return(
 		<div className="menuUsers">
 			<nav className="navUsers">
 				<NavLink className="navUsersOptions" to={FRONT_URL + "user:" + id}>
 					See profile
 				</NavLink>
-				<NavLink className="navUsersOptions" to={FRONT_URL + "chat:" + id}>
+{/*				<NavLink className="navUsersOptions" to={FRONT_URL + "chat:" + id}>
 					Send message
-				</NavLink>
-				<NavLink className="navUsersOptions"
-					to={ ( !isFriend ) ? ( BACK_URL + "add:" + id ) : ( BACK_URL + "remove:" + id ) }>
+				</NavLink> */}
+				<button className="navUsersOptions" onClick={ clickFriend }>
 					{ ( !isFriend ) ? "Add friend" : "Remove friend" }
-				</NavLink>
-				<NavLink className="navUsersOptions"
-					to={ (!isBlocked) ? ( BACK_URL + "block:" + id ) : ( BACK_URL + "unblock:" + id ) }>
+				</button>
+				<button className="navUsersOptions" onClick={ clickBlock }>
 					{ ( !isBlocked ) ? "Block user" : "Unblock user" }
-				</NavLink>
+				</button>
 			</nav>
 		</div>
 	);
