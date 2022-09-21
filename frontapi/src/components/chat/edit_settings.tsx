0c98bee035a127a17c771a67d55b4ce69dcf0e66
Matthieu CASUBOLO <macasubo@e1r13p6.clusters.42paris.fr>
2022-09-21 19:51:13 +0200

    delete channel front implement

--- frontapi/src/components/chat/edit_settings.tsx
+++ frontapi/src/components/chat/edit_settings.tsx
@@ -22,8 +22,10 @@ export function EditSettings ( props :IProps ) {
 	const [priv, setPriv] = useState< boolean >( props.room
 		&& ( props.room.state === "private" || props.room.state === "procated" )
 		? true : false );
+	const [passwordToggle, setPasswordToggle] = useState< boolean >( false );
 	const [password, setPassword] = useState< string >( "" );
 	const [nameError, setNameError] = useState< boolean >( false );
+	const [selectDelete, setSelectDelete] = useState< boolean >( false );
 
 	const nameChange = ( event :any ) => {
 		let value :string = event.target.value;
@@ -37,15 +39,27 @@ export function EditSettings ( props :IProps ) {
 		setPriv( event.target.checked );
 	};
 
+	const passwordToggleChange = ( event :any ) => {
+		setPasswordToggle( event.target.checked );
+	};
+
 	const passwordChange = ( event :any ) => {
 		let value :string = event.target.value;
 		setPassword( value );
 	};
 
-	const onSave = async() => {
+	const onSave = () => {
 		if( nameError )
 			return ;
+
+		chat_socket.socket.emit( "update-channel", { name: name, private: priv, password: (passwordToggle?password:null) }, ( response :any ) => {
+			console.log( response.data );
+		} );
 		props.close( false );
+	};
+
+	const onDelete = () => {
+		return ;
 	};
 
 	const adminChange = ( name? :string, uid? :string, isAdmin? :boolean ) => {
@@ -103,6 +117,16 @@ export function EditSettings ( props :IProps ) {
 					/>
 					</div>
 
+					<div>
+					<label id="edit-settings-input-toggle-pass-label">edit password</label>
+					<ToggleSwitch
+						id="edit-settings-input-toggle-pass"
+						checked={passwordToggle}
+						onChange={passwordToggleChange}
+					/>
+					</div>
+
+					{ passwordToggle &&
 					<Textinput
 						id="edit-settings-input-password"
 						placeholder="Password"
@@ -110,11 +134,18 @@ export function EditSettings ( props :IProps ) {
 						value={password}
 						style={{fontSize: '0.8em'}}
 						type="password"
-					/>					
+					/>
+					}					
 
 					<div id="edit-settings-button-save-parent">
 						<Button id="edit-settings-button-save"
 							value="save" fontSize={0.8} onClick={onSave} />
+						{ selectDelete 
+						? <div id="really-nah"><Button id="really" value="really?" fontSize={0.8} onClick={onDelete} />
+							<Button id="nah" value="nah" fontSize={0.8} onClick={() => { setSelectDelete( false ); } } /></div>
+						: <Button id="edit-settings-button-delete"
+							value="delete" fontSize={0.8} onClick={() => { setSelectDelete( true ); } } />
+						}
 					</div>
 
 				</div>
